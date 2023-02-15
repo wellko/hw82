84512eb97699d8cc0b87ed5d0babc24397ffc72b
Kwon Valerii <wellko24@gmail.com>
2023-02-15 15:27:21 +0600

    created users router and added it in index.ts

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors = require("cors");
 import artistRouter from "./routers/artists";
 import albumRouter from "./routers/albums";
 import trackRouter from "./routers/tracks";
+import usersRouter from "./routers/users";
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/artists', artistRouter);
 app.use('/albums', albumRouter);
 app.use('/tracks', trackRouter);
+app.use('users', usersRouter);
 
 const run = async () => {
     mongoose.set('strictQuery', false);

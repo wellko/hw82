92bc78b8d367c1b4eafaf493b285dd8183fd9356
Kwon Valerii <wellko24@gmail.com>
2023-02-27 16:16:36 +0600

    HW 86 added persist

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,18 +1,36 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {ArtistPageReducer} from "../features/ArtistPage/ArtistPageSlice";
 import {AlbumPageReducer} from "../features/AlbumPage/AlbumPageSlice";
 import {TrackPageReducer} from "../features/TracksPage/TrackPageSlice";
 import {UsersReducer} from "../features/users/UsersSlice";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 
+const usersPersistConfig = {
+	key: 'shop:users',
+	storage,
+	whitelist: ['user'],
+};
 
-export const store = configureStore({
-	reducer: {
+const rootReducer = combineReducers({
 		artists: ArtistPageReducer,
 		albums: AlbumPageReducer,
 		tracks: TrackPageReducer,
-		users: UsersReducer,
-	}
+		users: persistReducer(usersPersistConfig, UsersReducer),
 });
+
+export const store = configureStore({
+	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
-import {store} from "./app/store";
+import {persistor, store} from "./app/store";
 import {BrowserRouter} from "react-router-dom";
+import {PersistGate} from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Provider store={store}>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </PersistGate>
   </Provider>
 );
